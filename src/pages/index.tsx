d41164b4dc99bb3b4e9e89596d64524b172320d4
takeru.abe <takeru.abe@gaiax.com>
2022-09-24 23:58:34 +0900

    update README.md

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import squares from "sketches/squares";
 const Canvas = dynamic(() => import("sketches/canvas"), { ssr: false });
 
 const Home: NextPage = () => {
+
   return (
     <>
       <Canvas sketch={squares} />
