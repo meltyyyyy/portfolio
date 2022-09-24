7e20a6575ce220a9b35d9ae7263079fdd7b52d7e
takeru.abe <takeru.abe@gaiax.com>
2022-09-24 22:03:18 +0900

    update appTheme.ts

--- src/components/appTheme.ts
+++ src/components/appTheme.ts
@@ -12,7 +12,7 @@ const theme = createTheme({
     MuiCssBaseline: {
       styleOverrides: {
         body: {
-          backgroundColor: "#828282",
+          backgroundColor: "#EAEAEA",
         },
       },
     },
