a715aa1ed498063de4c9d3966492e28ee9899488
takeru.abe <takeru.abe@gaiax.com>
2022-09-24 21:06:07 +0900

    create appTheme.ts

---
+++ src/components/appTheme.tsx
@@ -0,0 +1,22 @@
+import { createTheme } from "@mui/material/styles";
+
+// -----------------------------------------------
+// MUI全体のテーマカラーを指定
+//
+// https://mui.com/customization/default-theme/
+// https://mui.com/customization/palette/
+// https://mui.com/customization/color/
+//
+const theme = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          backgroundColor: "#828282",
+        },
+      },
+    },
+  },
+});
+
+export default theme;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
-import type { AppProps } from 'next/app'
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "components/appTheme";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
 
-export default MyApp
+export default MyApp;
