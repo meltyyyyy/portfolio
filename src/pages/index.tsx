a4eb6dac54c897c3456624f33fb3d6565d519491
meltyyyyy <takeru.abe.g1@gmail.com>
2023-02-02 01:56:14 +0000

    create twitter icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Button, Modal } from "@mui/material";
 import { Box, Stack } from "@mui/system";
 import About from "components/about";
+import { TwitterIcon } from "components/appIcons";
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
 import { useState } from "react";
@@ -23,7 +24,7 @@ const Home: NextPage = () => {
         <Stack direction={"row"} justifyContent={"space-between"} alignItems="center">
           <Box sx={{ fontSize: 28, fontWeight: "bold" }}>TAKERU ABE</Box>
           <Stack direction={"row"}>
-            <Box sx={{ fontSize: 14, mx: 1 }}>SNS1</Box>
+            <TwitterIcon/>
             <Box sx={{ fontSize: 14, mx: 1 }}>SNS2</Box>
             <Box sx={{ fontSize: 14, ml: 1 }}>SNS2</Box>
           </Stack>
