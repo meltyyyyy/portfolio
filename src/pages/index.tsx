37566663ec4dc78158cee1e9e378530ac7f6f6e7
meltyyyyy <takeru.abe.g1@gmail.com>
2023-02-02 14:35:55 +0000

    design works

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,33 @@ const Home: NextPage = () => {
       >
         <Box sx={{ my: 12, mx: 16 }}>
           <Box sx={{ color: "#FFFFFF", fontSize: 40, mb: 4 }}>WORK</Box>
-          <Grid container spacing={2} justifyItems={"center"}>
+          <Grid container spacing={2} justifyItems={"center"} sx={{ my: 4 }}>
+            <Grid item xs={3}>
+              <Box sx={{ mx: 2 }}><Paper sx={{ my: 1, height: "160px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                Image
+              </Paper>
+                <Box sx={{ variant: "text", color: "#FFFFFF", my: 1 }}>Something</Box></Box>
+            </Grid>
+            <Grid item xs={3}>
+              <Box sx={{ mx: 2 }}><Paper sx={{ my: 1, height: "160px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                Image
+              </Paper>
+                <Box sx={{ variant: "text", color: "#FFFFFF", my: 1 }}>Something</Box></Box>
+            </Grid>
+            <Grid item xs={3}>
+              <Box sx={{ mx: 2 }}><Paper sx={{ my: 1, height: "160px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                Image
+              </Paper>
+                <Box sx={{ variant: "text", color: "#FFFFFF", my: 1 }}>Something</Box></Box>
+            </Grid>
+            <Grid item xs={3}>
+              <Box sx={{ mx: 2 }}><Paper sx={{ my: 1, height: "160px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                Image
+              </Paper>
+                <Box sx={{ variant: "text", color: "#FFFFFF", my: 1 }}>Something</Box></Box>
+            </Grid>
+          </Grid>
+          <Grid container spacing={2} justifyItems={"center"} sx={{ my: 4 }}>
             <Grid item xs={3}>
               <Box sx={{ mx: 2 }}><Paper sx={{ my: 1, height: "160px", display: "flex", alignItems: "center", justifyContent: "center" }}>
                 Image
