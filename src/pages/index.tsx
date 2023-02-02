19d9367e13a1dfb34784607ce4fe9032c184991a
meltyyyyy <takeru.abe.g1@gmail.com>
2023-02-02 14:33:46 +0000

    add works

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Link, Modal } from "@mui/material";
+import { Button, Link, Modal, Grid, Paper } from "@mui/material";
 import { Box, Stack } from "@mui/system";
 import { FacebookUrl, GitHubUrl, LinkedInUrl, TwitterUrl } from "common/constants";
 import About from "components/about";
@@ -11,6 +11,7 @@ const Canvas = dynamic(() => import("sketches/canvas"), { ssr: false });
 
 const Home: NextPage = () => {
   const [openAbout, setOpenAbout] = useState<boolean>(false);
+  const [openWork, setOpenWork] = useState<boolean>(false);
 
   return (
     <>
@@ -20,6 +21,40 @@ const Home: NextPage = () => {
         onClose={() => setOpenAbout(false)}
       >
         <About />
+      </Modal>
+      <Modal
+        open={openWork}
+        onClose={() => setOpenWork(false)}
+      >
+        <Box sx={{ my: 12, mx: 16 }}>
+          <Box sx={{ color: "#FFFFFF", fontSize: 40, mb: 4 }}>WORK</Box>
+          <Grid container spacing={2} justifyItems={"center"}>
+            <Grid item xs={3}>
+              <Box sx={{ mx: 2 }}><Paper sx={{ my: 1, height: "160px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                Image
+              </Paper>
+                <Box sx={{ variant: "text", color: "#FFFFFF", my: 1 }}>Something</Box></Box>
+            </Grid>
+            <Grid item xs={3}>
+              <Box sx={{ mx: 2 }}><Paper sx={{ my: 1, height: "160px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                Image
+              </Paper>
+                <Box sx={{ variant: "text", color: "#FFFFFF", my: 1 }}>Something</Box></Box>
+            </Grid>
+            <Grid item xs={3}>
+              <Box sx={{ mx: 2 }}><Paper sx={{ my: 1, height: "160px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                Image
+              </Paper>
+                <Box sx={{ variant: "text", color: "#FFFFFF", my: 1 }}>Something</Box></Box>
+            </Grid>
+            <Grid item xs={3}>
+              <Box sx={{ mx: 2 }}><Paper sx={{ my: 1, height: "160px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                Image
+              </Paper>
+                <Box sx={{ variant: "text", color: "#FFFFFF", my: 1 }}>Something</Box></Box>
+            </Grid>
+          </Grid>
+        </Box>
       </Modal>
       <Box sx={{ m: 2 }}>
         <Stack direction={"row"} justifyContent={"space-between"} alignItems="center">
@@ -81,30 +116,34 @@ const Home: NextPage = () => {
             }}
             variant="text"
           >ABOUT</Button>
-          <Button sx={{
-            color: "#000000",
-            '&:hover': {
-              background: "transparent",
-              color: '#797979'
-            },
-            fontSize: 14,
-            mx: 1,
-            minHeight: 0,
-            minWidth: 0,
-            padding: 0
-          }} variant="text">WORK</Button>
-          <Button sx={{
-            color: "#000000",
-            '&:hover': {
-              background: "transparent",
-              color: '#797979'
-            },
-            fontSize: 14,
-            mx: 1,
-            minHeight: 0,
-            minWidth: 0,
-            padding: 0
-          }} variant="text">CONTACT</Button>
+          <Button
+            onClick={() => setOpenWork(true)}
+            sx={{
+              color: "#000000",
+              '&:hover': {
+                background: "transparent",
+                color: '#797979'
+              },
+              fontSize: 14,
+              mx: 1,
+              minHeight: 0,
+              minWidth: 0,
+              padding: 0
+            }} variant="text">WORK</Button>
+          <Button
+            onClick={() => window.open("mailto:takeru.abe.g1@gmail.com?subject=Subject&body=Body%20goes%20here")}
+            sx={{
+              color: "#000000",
+              '&:hover': {
+                background: "transparent",
+                color: '#797979'
+              },
+              fontSize: 14,
+              mx: 1,
+              minHeight: 0,
+              minWidth: 0,
+              padding: 0
+            }} variant="text">CONTACT</Button>
         </Stack>
       </Box>
     </>
