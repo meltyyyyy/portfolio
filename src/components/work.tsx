5e63968bcf4da3962ede82744dfeccdd68f2ee60
meltyyyyy <takeru.abe.g1@gmail.com>
2023-02-03 06:21:01 +0000

    yarn add @mui/icons-material

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -1,11 +1,15 @@
-import { Box, Grid, Paper } from "@mui/material";
+import { Box, Grid, IconButton, Paper, Stack } from "@mui/material";
 import { forwardRef } from "react";
+import CloseIcon from '@mui/icons-material/Close';
 
 const Work = (props: { forwardRef: React.Ref<HTMLElement> }) => {
   const { forwardRef } = props;
   return (
     <Box sx={{ my: 12, mx: 16 }} ref={forwardRef} tabIndex={-1}>
-      <Box sx={{ color: "#FFFFFF", fontSize: 40, mb: 4 }}>WORK</Box>
+      <Stack direction={"row"} justifyContent={"space-between"}>
+        <Box sx={{ color: "#FFFFFF", fontSize: 40, mb: 4 }}>WORK</Box>
+        <IconButton><CloseIcon /></IconButton>
+      </Stack>
       <Grid container spacing={2} justifyItems={"center"} sx={{ my: 4 }}>
         <Grid item xs={3}>
           <Box sx={{ mx: 2 }}>
